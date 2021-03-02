5824f883953fd3d357c2771fdafb7c11b9ffd98f
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2021-03-02 09:45:48 +0200

    Удалили app.UseRouting(). Маршрутизации нет. Добавили StatusCodePages, StaticFiles и использование пуи запроса для запуска контроллера/действия

--- ASPGoshaDudar/Startup.cs
+++ ASPGoshaDudar/Startup.cs
@@ -21,20 +21,11 @@ namespace ASPGoshaDudar
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
